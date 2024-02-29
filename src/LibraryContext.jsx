4526f26486aba16436dcf8d483b96c1c81c40840
Junier Bolivar <junierba@yahoo.co.uk>
2024-02-29 11:12:23 +0100

    add a comment

--- src/LibraryContext.jsx
+++ src/LibraryContext.jsx
@@ -1,37 +1,29 @@
-import { createContext, useContext, useState } from "react";
-import { collection } from "./collection";
+import { createContext, useContext, useState } from 'react';
+import { collection } from './collection';
 
 export const LibraryContext = createContext({});
 
-LibraryContext.displayName = "LibraryContext";
+LibraryContext.displayName = 'LibraryContext';
 
 export const LibraryContextProvider = ({ children }) => {
   const [books, setBooks] = useState(collection.books);
   const borrowBook = (id) => {
-    const newBooks = books.map((book) =>
-      book.id === id ? { ...book, available: false } : book
-    );
+    const newBooks = books.map((book) => (book.id === id ? { ...book, available: false } : book));
     return setBooks(newBooks);
   };
   const returnBook = (id) => {
-    const newBooks = books.map((book) =>
-      book.id === id ? { ...book, available: true } : book
-    );
+    const newBooks = books.map((book) => (book.id === id ? { ...book, available: true } : book));
     return setBooks(newBooks);
   };
 
-  return (
-    <LibraryContext.Provider value={{ books, borrowBook, returnBook }}>
-      {children}
-    </LibraryContext.Provider>
-  );
+  return <LibraryContext.Provider value={{ books, borrowBook, returnBook }}>{children}</LibraryContext.Provider>;
 };
 
+//-----This is the custom HOOK:❗ using the keyword "use" as prefix.
 export const useLibrary = () => {
   const context = useContext(LibraryContext);
   if (!context) {
-    throw new Error("useLibrary must be used within a LibraryContextProvider");
+    throw new Error('useLibrary must be used within a LibraryContextProvider');
   }
   return context;
 };
-    
